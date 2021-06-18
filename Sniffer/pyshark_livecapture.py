b2d8c01873e3b4d4beb82853a7a1e49dbac09afc
Vedant Lanjewar <vedant.edu9@gmail.com>
2021-06-18 10:30:49 +0530

    Added comments
    Details about which argument to use.

--- Sniffer/pyshark_livecapture.py
+++ Sniffer/pyshark_livecapture.py
@@ -1,4 +1,7 @@
 import pyshark
+# in the interface argument, put you interface through which packets come
+# you can check it by opening wireshark, and the first option you would have to select your interface
+# that same interface name you have input in the argument
 capture = pyshark.LiveCapture(interface='eth0')
 capture.sniff(timeout=50)
 capture
