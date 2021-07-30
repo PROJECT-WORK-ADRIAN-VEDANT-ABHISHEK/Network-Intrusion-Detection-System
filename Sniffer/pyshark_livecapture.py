5c57c6ddfda84fd42c5c7e2505841be444743c0b
Vedant Lanjewar <vedant.edu9@gmail.com>
2021-07-30 19:47:56 +0530

    Packet extraction Details

--- Sniffer/pyshark_livecapture.py
+++ Sniffer/pyshark_livecapture.py
@@ -2,6 +2,6 @@ import pyshark
 # in the interface argument, put you interface through which packets come
 # you can check it by opening wireshark, and the first option you would have to select your interface
 # that same interface name you have input in the argument
-capture = pyshark.LiveCapture(interface='eth0')
-capture.sniff(timeout=50)
+capture = pyshark.LiveCapture(interface='WI-Fi')
+capture.sniff(timeout=2)
 capture
