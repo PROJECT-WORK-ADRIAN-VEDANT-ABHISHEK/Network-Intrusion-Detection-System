71c364932b8f74bbffe6f5886647920fd8b2f7f0
Vedant Lanjewar <vedant.edu9@gmail.com>
2021-10-01 21:48:24 +0530

    Correcting commit

--- Sniffer/pyshark_livecapture.py
+++ Sniffer/pyshark_livecapture.py
@@ -3,7 +3,7 @@ import pyshark
 # you can check it by opening wireshark, and the first option you would have to select your interface
 # that same interface name you have input in the argument
 capture = pyshark.LiveCapture(interface='Wi-Fi')
-capture.sniff(timeout=5)
+capture.sniff(timeout=2)
 print(capture)
 
 capture[0]
