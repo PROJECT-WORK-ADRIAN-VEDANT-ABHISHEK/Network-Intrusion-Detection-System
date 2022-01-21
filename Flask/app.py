1f950f68eb3da2077a6f49a1f37b9c9e5d65f4a4
adrian2504 <60578726+adrian2504@users.noreply.github.com>
2022-01-21 20:14:32 +0530

    stored selection result in variable

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, request
 import psutil
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/',methods=["POST","GET"])
 def interface_option():
+    val1=" "
     interface_list=[]
     addrs = psutil.net_if_addrs()
     for x in addrs.keys():
@@ -13,9 +14,10 @@ def interface_option():
     if request.method=="GET":
         return render_template("index.html", interface_list=interface_list)
     else:
-        selec=request.form.get('interfaces')
+        selec =request.form.get('interfaces')
+        print(str(selec))
         return str(selec)
-    
 
 if __name__== "__main__":
     app.run(debug=True)
+
